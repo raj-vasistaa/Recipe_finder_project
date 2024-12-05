e59fece57e7763957a0565dcbacd2906e0f22ee9
Rajendra A <rajendraprince2004@gmail.com>
2024-12-05 18:48:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,108 +1,32 @@
-const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
 
-document.querySelector("#enter").addEventListener("click", () => {
-  const item = document.querySelector("#item")
-  createItem(item)
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
 })
 
-document.querySelector("#item").addEventListener("keypress", (e) => {
-  if(e.key === "Enter"){
-    const item = document.querySelector("#item")
-    createItem(item)
-  }
-})
-
-function displayDate(){
-  let date = new Date()
-  date = date.toString().split(" ")
-  date = date[1] + " " + date[2] + " " + date[3] 
-  document.querySelector("#date").innerHTML = date 
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=7aa516a5&app_key=dc836a223fb788b11ae390504d9e97ce&from=0&to=10`);
+    const data = await response.json();
+    displayRecipes(data.hits);
 }
 
-function displayItems(){
-  let items = ""
-  for(let i = 0; i < itemsArray.length; i++){
-    items += `<div class="item">
-                <div class="input-controller">
-                  <textarea disabled>${itemsArray[i]}</textarea>
-                  <div class="edit-controller">
-                    <i class="fa-solid fa-check deleteBtn"></i>
-                    <i class="fa-solid fa-pen-to-square editBtn"></i>
-                  </div>
-                </div>
-                <div class="update-controller">
-                  <button class="saveBtn">Save</button>
-                  <button class="cancelBtn">Cancel</button>
-                </div>
-              </div>`
-  }
-  document.querySelector(".to-do-list").innerHTML = items
-  activateDeleteListeners()
-  activateEditListeners()
-  activateSaveListeners()
-  activateCancelListeners()
-}
-
-function activateDeleteListeners(){
-  let deleteBtn = document.querySelectorAll(".deleteBtn")
-  deleteBtn.forEach((dB, i) => {
-    dB.addEventListener("click", () => { deleteItem(i) })
-  })
-}
-
-function activateEditListeners(){
-  const editBtn = document.querySelectorAll(".editBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  editBtn.forEach((eB, i) => {
-    eB.addEventListener("click", () => { 
-      updateController[i].style.display = "block"
-      inputs[i].disabled = false })
-  })
-}
-
-function activateSaveListeners(){
-  const saveBtn = document.querySelectorAll(".saveBtn")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  saveBtn.forEach((sB, i) => {
-    sB.addEventListener("click", () => {
-      updateItem(inputs[i].value, i)
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
     })
-  })
+    resultsList.innerHTML = html;
 }
-
-function activateCancelListeners(){
-  const cancelBtn = document.querySelectorAll(".cancelBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  cancelBtn.forEach((cB, i) => {
-    cB.addEventListener("click", () => {
-      updateController[i].style.display = "none"
-      inputs[i].disabled = true
-      inputs[i].style.border = "none"
-    })
-  })
-}
-
-function createItem(item){
-  itemsArray.push(item.value)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-function deleteItem(i){
-  itemsArray.splice(i,1)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-function updateItem(text, i){
-  itemsArray[i] = text
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-window.onload = function() {
-  displayDate()
-  displayItems()
-};
